058dfedf486f38b5a98ae76110ebd5849aef0421
yanghy <snxamdf@126.com>
2025-12-30 16:20:37 +0800

    energy webview

--- windows/wv_init.go
+++ windows/wv_init.go
@@ -12,23 +12,10 @@ package windows
 
 import (
 	"github.com/energye/lcl/api"
-	"github.com/energye/lcl/emfs"
-	"github.com/energye/lcl/lcl"
-	"os"
 )
 
 // Init Webview2
-func Init(libs emfs.IEmbedFS, resources emfs.IEmbedFS) {
-	lcl.Init(libs, resources)
-	defer func() {
-		if err := recover(); err != nil {
-			println(err)
-			os.Exit(1)
-		}
-	}()
-	//runtime.LockOSThread()
-	//defer runtime.UnlockOSThread()
-
+func Init() {
 	// 注册 Webview2 对象事件回调
 	api.SetEventCallback(eventCallback, api.EctWV)
 	// 注册 Webview2 对象事件回调移除
