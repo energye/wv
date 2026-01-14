c3e0662f0efd4c4a151acd68cdef50a5bb0af467
yanghy <snxamdf@126.com>
2026-01-14 23:41:00 +0800

    energy webkit2

---
+++ darwin/wkpreferences_predef_darwin.go
@@ -0,0 +1,40 @@
+//----------------------------------------
+//
+// Copyright © yanghy. All Rights Reserved.
+//
+// Licensed under Apache License Version 2.0, January 2004
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+//----------------------------------------
+
+//go:build darwin
+
+// :predefine:
+
+package darwin
+
+/*
+#cgo CFLAGS: -mmacosx-version-min=10.15 -x objective-c
+#cgo LDFLAGS: -mmacosx-version-min=10.15 -framework Cocoa
+
+#include "WebKit/WebKit.h"
+
+void EnableDeveloperExtras(void* wkPreferences) {
+    WKPreferences* preferences = (WKPreferences*)wkPreferences;
+	[preferences setValue:@YES forKey:@"developerExtrasEnabled"];
+}
+*/
+import "C"
+import (
+	wvTypes "github.com/energye/wv/types/darwin"
+	"unsafe"
+)
+
+func EnableDevtools(wkPreferences wvTypes.WKPreferences) {
+	C.EnableDeveloperExtras(unsafe.Pointer(wkPreferences))
+}
+
+func (m *TWkPreferences) EnableDevtools() {
+	EnableDevtools(m.Data())
+}
